12a74ea44e1add17ab423a841ebd8cca0b3be9ab
Haley Parks <haleyparks329@gmail.com>
2026-08-18 09:17:50 +0200

    Harden Substack feed builds

---
+++ src/data/substack-feed-cache.ts
@@ -0,0 +1,19 @@
+export const cachedSubstackPosts = [
+  {
+    title: "Questions I’ve Been Asking Myself",
+    url: "https://haleyparks329.substack.com/p/questions-ive-been-asking-myself",
+    publishedAt: "2026-08-17T17:14:24.000Z",
+    excerpt: "On work, ambition, and figuring out where I fit",
+    image:
+      "https://substackcdn.com/image/fetch/$s_!Lap3!,w_256,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa2f1ecff-0de6-4176-88a6-d2b09407e80c_400x400.jpeg",
+  },
+  {
+    title: "Why is the Human Still the Orchestration Layer?",
+    url: "https://haleyparks329.substack.com/p/why-is-the-human-still-the-orchestration",
+    publishedAt: "2026-08-12T17:46:19.000Z",
+    excerpt:
+      "My biggest gripe with software today is that I am still doing the work.",
+    image:
+      "https://substackcdn.com/image/fetch/$s_!Lap3!,w_256,c_limit,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2Fa2f1ecff-0de6-4176-88a6-d2b09407e80c_400x400.jpeg",
+  },
+] as const;
